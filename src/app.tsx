3ab77b4835fbd2a410f90968f2267dfe2bdbd555
zhou_quan <zhouquan@lenovoconnect.com>
2019-09-27 17:49:22 +0800

    add todolist

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 export function App() {
-    const [todo, settodo] = React.useState('');
+    const [todos, settodo] = React.useState([]);
+    const addTodoList = (e) => {
+        if (e.keyCode === 13) {
+            settodo([...todos, e.currentTarget.value]);
+        }
+    };
     return (
         <>
             <div className="inputWrap">
-                <input type="text" onChange={(e) => { settodo(e.currentTarget.value) }} />
+                <input type="text" onKeyDown={(e) => { addTodoList(e) }} />
             </div>
             <div className="list">
                 <ul>
-                    <li>{todo}</li>
+                    {
+                        todos.map((item, index) => (
+                            <li key={index}><span className="checkSpan"></span><p>{item}</p></li>
+                        ))
+                    }
                 </ul>
             </div>
         </>

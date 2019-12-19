40561b5dc89a85adc46a119fa4ebe7548ee4011e
zhou_quan <zhouquan@lenovoconnect.com>
2019-12-19 16:53:06 +0800

    add useFetch

---
+++ src/utils/useFetch.ts
@@ -0,0 +1,42 @@
+import { useState, useCallback, useEffect } from 'react';
+import axios from 'axios';
+
+export function useFetch(params: any, visible = true) {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(false);
+    // 缓存参数
+    const [newParams, setNewParams] = useState(params);
+    const fetchApi = useCallback(async () => {
+        // console.log("useCallback");
+        if (visible) {
+            setLoading(true);
+            // console.log("fetch===>");
+            const res = await axios(newParams);
+            if (res.code === 1) {
+                setData(res.data);
+            }
+            setLoading(false);
+        }
+    }, [fetch, newParams, visible]);
+
+    useEffect(() => {
+        // console.log("useEffect");
+        fetchApi();
+    }, [fetchApi]);
+    const doFetch = useCallback(
+        (rest) => {
+            setNewParams({ ...newParams, ...(rest || {}) });
+        },
+        [newParams]
+    );
+
+    const reFetch = () => {
+        setNewParams({...newParams});
+    };
+    return {
+        loading,
+        data,
+        doFetch,
+        reFetch
+    };
+}

447605419c1a2184d7b8b51fec1f650ca045bba1
zhou_quan <zhouquan@lenovoconnect.com>
2020-01-13 17:22:55 +0800

    测试工具

--- src/router/config.ts
+++ src/router/config.ts
@@ -3,7 +3,7 @@ import { lazy } from 'react';
 import BasicLayout from '@src/layouts/BasicLayout';
 import BlankLayout from '@src/layouts/BlankLayout';
 
-// import TodoList from '../pages/todoList/TodoList';
+import TodoList from '../pages/todoList/TodoList';
 // import MainPage from '../pages/index/Main';
 // import { BASE_URL } from '../constants/index';
 
@@ -11,13 +11,13 @@ const routes = [
     {
         path: '/',
         component: BlankLayout, // 空白页布局
-        breadcrumbName:'',
+        breadcrumbName: '',
         children: [
             // 子菜单路由
             {
                 path: '/login', // 路由路径
                 name: '登录页', // 菜单名称 (不设置,则不展示在菜单栏中）
-                breadcrumbName:'登录页',
+                breadcrumbName: '登录页',
                 icon: 'setting', // 菜单图标
                 component: lazy(() => import('@src/pages/Login')) // 懒加载 路由组件
             },
@@ -25,19 +25,19 @@ const routes = [
                 path: '/',
                 // exact: true,
                 component: BasicLayout, // 基本布局
-                breadcrumbName:'',
+                breadcrumbName: '',
                 children: [
                     {
                         path: '/home',
                         name: 'home主页',
-                        breadcrumbName:'home',
+                        breadcrumbName: 'home',
                         icon: 'home',
                         component: lazy(() => import('@src/pages/Home/index'))
                     },
                     {
                         path: '/exception',
                         name: '异常页',
-                        breadcrumbName:'异常页',
+                        breadcrumbName: '异常页',
                         // exact: true,
                         icon: 'warning',
                         children: [
@@ -62,8 +62,8 @@ const routes = [
                             }
                         ]
                     },
-                    { path: '/', exact: true, redirect: '/home',breadcrumbName:'home', },
-                    { path: '*', exact: true, redirect: '/exception/404',breadcrumbName:'404', }
+                    { path: '/', exact: true, redirect: '/home', breadcrumbName: 'home' },
+                    { path: '*', exact: true, redirect: '/exception/404', breadcrumbName: '404' }
                 ]
             }
         ]
